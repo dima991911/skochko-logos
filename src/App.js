9dc0267142cd55b133643db7793b484cb84a3845
Dima Skochko <dimon4uk.ds@gmail.com>
2020-07-28 18:21:43 +0300

    add our projects, fix animate title in top section

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React, { useRef, useEffect } from "react";
+import React, {useRef, useEffect, useState} from "react";
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import "./App.css";
 
-import jannata from './images/works/jannata2.jpg';
-import apprentice from './images/works/apprentice-search.jpg';
-import bodyshop from './images/works/bodyshop.jpg';
-import clorova from './images/works/clorova.jpg';
-import prodotti from './images/works/prodotti.jpg';
-import raia from './images/works/raia.jpg';
-import relationshift from './images/works/relationshift.jpg';
-import vold from './images/works/vold.jpg';
+import beard from './images/works/01_beardstyle_800x900.jpg';
+import prospervideo from './images/works/02_prospervideo_800x900.jpg';
+import ditchingdisponsable from './images/works/03_ditchingdisponsable_800x900.jpg';
+import emilyjrealty from './images/works/04_emilyjrealty_800x900.jpg';
+import igp from './images/works/05_igp_800x900.jpg';
+import lemontnoir from './images/works/06_lemontnoir_800x900.jpg';
+import sakseide from './images/works/07_sakseide_800x900.jpg';
+import mainevent from './images/works/08_mainevent_800x900.jpg';
+import aaronanthony from './images/works/09_aaronanthony_800x900.jpg';
+import britishloghomes from './images/works/10_britishloghomes_800x900.jpg';
+import starsandbones from './images/works/11_starsandbones_800x900.jpg';
+import archx from './images/works/12_archx_800x900.jpg';
 
 const works = [
     [
-        { avatarUrl: jannata, name: 'Jonnata', description: 'Revolutionizing the Nightlife Experience.' },
-        { avatarUrl: bodyshop, name: 'Bodyshop', description: 'Revolutionizing the Nightlife Experience.' },
+        { avatarUrl: beard, name: 'BEARD STYLE', description: 'Helping men grow and groom awesome beards' },
+        { avatarUrl: prospervideo, name: 'PROSPER.VIDEO', description: 'Mobile-first video production studio' },
     ],
     [
-        { avatarUrl: clorova, name: 'Clorova', description: 'Revolutionizing the Nightlife Experience.' },
-        { avatarUrl: apprentice, name: 'Apprentice', description: 'Revolutionizing the Nightlife Experience.' },
+        { avatarUrl: ditchingdisponsable, name: 'DITCHING DISPONSABLES', description: 'You make a difference' },
+        { avatarUrl: emilyjrealty, name: 'EMILY J', description: 'Real estate company' },
     ],
     [
-        { avatarUrl: prodotti, name: 'Prodotti', description: 'Revolutionizing the Nightlife Experience.' },
-        { avatarUrl: raia, name: 'Raia', description: 'Revolutionizing the Nightlife Experience.' },
+        { avatarUrl: igp, name: 'INSTANT GLOBAL PAYMENTS', description: 'Send money to any country with world-class exchange rates' },
+        { avatarUrl: lemontnoir, name: 'LE MONTNOIR', description: 'Fine furniture' },
     ],
     [
-        { avatarUrl: relationshift, name: 'Relationshift', description: 'Revolutionizing the Nightlife Experience.' },
-        { avatarUrl: vold, name: 'Vold', description: 'Revolutionizing the Nightlife Experience.' },
+        { avatarUrl: sakseide, name: 'SAKSEIDE CONSULTING', description: 'Change management, organizational psychology' },
+        { avatarUrl: mainevent, name: 'MAIN EVENT CATERING', description: 'Voted Anchorage, “Alaska’s Best Caterer”' },
+    ],
+    [
+        { avatarUrl: aaronanthony, name: 'AARON ANTHONY', description: '' },
+        { avatarUrl: britishloghomes, name: 'BRITISH LOG HOMES', description: 'The home of handcrafted log cabins in the UK' },
+    ],
+    [
+        { avatarUrl: starsandbones, name: 'STARS&BONES', description: 'Audiovisual production company' },
+        { avatarUrl: archx, name: 'ARCH X', description: 'Residential architectural design' },
     ],
 ];
 
@@ -82,40 +94,45 @@ function WorkComponent({ work }) {
 }
 
 function App() {
+    const [canAnimateTitle, setAnimateTitle] = useState(false);
 
-    // useEffect(() => {
-    //     const smoothScroll = (e) => {
-    //         window.scrollBy({
-    //             top: e.deltaY,
-    //             left: 0,
-    //             behavior: 'smooth'
-    //         });
-    //     };
-    //
-    //     document.body.addEventListener('wheel', smoothScroll);
-    //
-    //     return () => document.body.removeEventListener('wheel', smoothScroll);
-    // }, []);
+    useEffect(() => {
+        const scrollToTop = () => {
+            window.scrollTo(0 ,0);
+        };
+
+        const animateTitle = () => {
+            setAnimateTitle(true);
+        };
+
+        window.addEventListener('beforeunload', scrollToTop);
+        window.addEventListener('load', animateTitle);
+
+        return () => {
+            window.removeEventListener('beforeunload', scrollToTop);
+            window.removeEventListener('load', animateTitle);
+        }
+    }, []);
 
     return (
         <ParallaxProvider>
             <div className="page">
                 <div className="top-section-wrapper">
                     <header className="header">
-                        <div className="header-wrapper">
-                            <div className="header-logo">Logo</div>
-                            <div className="header-link">Some link</div>
-                        </div>
+                        <div className="header-logo">Logo</div>
+                        <div className="header-link">Some link</div>
                     </header>
 
                     <section className="top-section">
-                        <h1>
-                            {topSectionTitles.map((title, index) => (
-                                <span className="top-section-span-animate-wrapper" key={index}>
-                                <span className="top-section-span" style={{ animationDuration: `${(index * 0.4) + 0.7}s` }}>{title}</span>
-                            </span>
-                            ))}
-                        </h1>
+                        {canAnimateTitle && (
+                            <h1>
+                                {topSectionTitles.map((title, index) => (
+                                    <span className="top-section-span-animate-wrapper" key={index}>
+                                        <span className="top-section-span" style={{ animationDuration: `${(index * 0.3) + 0.6}s` }}>{title}</span>
+                                    </span>
+                                ))}
+                            </h1>
+                        )}
                     </section>
                 </div>
 
