d1e12b45f9c92e4aa4309a03294013a74feaa285
Dima Skochko <dimon4uk.ds@gmail.com>
2020-08-04 12:27:40 +0300

    add new projects

--- src/images/works/index.js
+++ src/images/works/index.js
@@ -1,15 +1,28 @@
-import beard from './01_beardstyle_800x900.jpg';
-import prospervideo from './02_prospervideo_800x900.jpg';
-import ditchingdisponsable from './03_ditchingdisponsable_800x900.jpg';
-import emilyjrealty from './04_emilyjrealty_800x900.jpg';
+import beard from './01_beard style_800x900.jpg';
+import prospervideo from './02_prosper.video_800x900.jpg';
+import ditchingdisponsable from './03_ditching disponsables_800x900.jpg';
+import emilyjrealty from './04_emily j_800x900.jpg';
 import igp from './05_igp_800x900.jpg';
-import lemontnoir from './06_lemontnoir_800x900.jpg';
+import lemontnoir from './06_le montnoir_800x900.jpg';
 import sakseide from './07_sakseide_800x900.jpg';
-import mainevent from './08_mainevent_800x900.jpg';
-import aaronanthony from './09_aaronanthony_800x900.jpg';
-import britishloghomes from './10_britishloghomes_800x900.jpg';
-import starsandbones from './11_starsandbones_800x900.jpg';
-import archx from './12_archx_800x900.jpg';
+import mainevent from './08_main event_800x900.jpg';
+import aaronanthony from './09_aaron anthony_800x900.jpg';
+import britishloghomes from './10_british log homes_800x900.jpg';
+import archx from './11_arch x_800x900.jpg';
+import starsandbones from './12_stars and bones_800x900.jpg';
+import egoist from './13_egoist_800x900.jpg';
+import prepEd from './14_prep ed_800x900.jpg';
+import etnoCity from './15_etno city_800x900.jpg';
+import simGolf from './16_sim.golf_800x900.jpg';
+import north88 from './17_north88_800x900.jpg';
+import mountMercy from './18_mount mercy_800x900.jpg';
+import pigeonFilm from './19_pigeon film_800x900.jpg';
+import aurumNovum from './20_aurum novum_800x900.jpg';
+import bigThinkers from './21_big thinkers_800x900.jpg';
+import aceTheExam from './22_ace the exam_800x900.jpg';
+import drawMarket from './23_draw market_800x900.jpg';
+import activeTravel from './24_active travel_800x900.jpg';
+import greenStreet from './25_green street_800x900.jpg';
 
 export const works = [
     [
@@ -29,11 +42,38 @@ export const works = [
         { avatarUrl: mainevent, name: 'MAIN EVENT CATERING', description: 'Voted Anchorage, “Alaska’s Best Caterer”' },
     ],
     [
-        { avatarUrl: aaronanthony, name: 'AARON ANTHONY', description: '' },
+        { avatarUrl: aaronanthony, name: 'AARON ANTHONY', description: 'Emerging author, financial management expert, and aspiring creative director' },
         { avatarUrl: britishloghomes, name: 'BRITISH LOG HOMES', description: 'The home of handcrafted log cabins in the UK' },
     ],
     [
         { avatarUrl: starsandbones, name: 'STARS&BONES', description: 'Audiovisual production company' },
         { avatarUrl: archx, name: 'ARCH X', description: 'Residential architectural design' },
+    ],
+    [
+        { avatarUrl: egoist, name: 'EGOIST', description: 'Night club' },
+        { avatarUrl: prepEd, name: 'PREP.ED', description: 'Social education project' },
+    ],
+    [
+        { avatarUrl: etnoCity, name: 'ETNO CITY', description: 'Ethnocultural clothing project' },
+        { avatarUrl: simGolf, name: 'SIM.GOLF', description: 'Private golf organization' },
+    ],
+    [
+        { avatarUrl: north88, name: 'NORTH 88 OUTDOOR', description: 'Luxury outdoor furniture company' },
+        { avatarUrl: mountMercy, name: 'MOUNT MERCY', description: 'Where Adam met Eve' },
+    ],
+    [
+        { avatarUrl: pigeonFilm, name: 'PIGEON FILM', description: 'Audiovisual production company' },
+        { avatarUrl: aurumNovum, name: 'AURUM NOVUM', description: 'Jewelry company' },
+    ],
+    [
+        { avatarUrl: bigThinkers, name: 'BIG THINKERS', description: 'Whatever you\'re thinking, think bigger' },
+        { avatarUrl: aceTheExam, name: 'ACE THE EXAM', description: 'Online preparation material for postgraduate surgical examinations' },
+    ],
+    [
+        { avatarUrl: drawMarket, name: 'DRAW MARKET', description: 'Online artworks market' },
+        { avatarUrl: activeTravel, name: 'ACTIVE TRAVEL MARKETING', description: 'Traveling company' },
+    ],
+    [
+        { avatarUrl: greenStreet, name: 'GREEN STREET CREATIVE', description: 'Marketing creative company' },
     ],
 ];
