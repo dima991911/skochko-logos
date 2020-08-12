e24c28f02d4867c25a06707fed4e6198f8c34b8b
Dima <dimon4uk.ds@gmail.com>
2020-08-12 14:38:49 +0300

    create auth middleware

--- backend/routes/api/projects/index.js
+++ backend/routes/api/projects/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer();
+const isAuthMiddleware = require('../../../middleware/auth.middleware');
 
 const router = express.Router();
 
 const presentationController = require('../../../controllers/presentation.controller');
 
-router.post('/',
+router.post('/', isAuthMiddleware,
     upload.fields([{ name: 'topSectionImg', maxCount: 1 }, { name: 'bottomSectionImg', maxCount: 1 }, { name: 'preview', maxCount: 1 }, { name: 'images', maxCount: 10 }]),
     presentationController.createPresentation);
 
 router.get('/', presentationController.getProjects);
 router.get('/:slug', presentationController.getProject);
-router.delete('/:id', presentationController.removeProject);
+router.delete('/:id', isAuthMiddleware, presentationController.removeProject);
 
 module.exports = router;
