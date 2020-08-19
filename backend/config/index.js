9fb3bde6c3fefcced1193ec0470dcb0fbad7792c
Dima <dimon4uk.ds@gmail.com>
2020-08-19 17:53:26 +0300

    hide bucket info

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,9 +3,9 @@ const config = {
         secret: 'EF5A90CD2DFE082D6D2615BDE8F046B896DDD0278CD9EC718C99DFCA3B66E755'
     },
     bucket: {
-        BUCKET_NAME: 'skochko',
-        USER_KEY: 'AKIAUN26YRIAVLITN5NM',
-        USER_SECRET: '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',
+        BUCKET_NAME: process.env.BUCKET_NAME,
+        USER_KEY: process.env.USER_KEY,
+        USER_SECRET: process.env.USER_SECRET,
     },
 };
 
