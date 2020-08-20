3b4e56ebcf96191db2492d4d23c46b750f3be4fc
Dima <dimon4uk.ds@gmail.com>
2020-08-20 14:02:53 +0300

    edit config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-    api: 'http://localhost:8080/api/',
+    api: (process.env.API_URL || 'http://localhost:8080/') + 'api/',
     publicApiForImages: 'http://localhost:8080/',
     jwt: {
         secret: 'EF5A90CD2DFE082D6D2615BDE8F046B896DDD0278CD9EC718C99DFCA3B66E755',
